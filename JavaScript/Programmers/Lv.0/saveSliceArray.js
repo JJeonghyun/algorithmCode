aa3039e2dc82397abbe7fd8cea66784bc45c5c21
JJeonghyun <ghkdwja9649@gmail.com>
2024-05-18 15:08:55 +0900

    update: daily Coding

---
+++ JavaScript/Programmers/Lv.0/saveSliceArray.js
@@ -0,0 +1,11 @@
+function solution(my_str, n) {
+    let result = [];
+    let sliceNum = Math.floor(my_str.length / n);
+    for(let i = 0 ; i < sliceNum ; i++){
+        result.push(my_str.slice(i*n, (i+1) * n))
+    }
+    if(my_str.length % n) result.push(my_str.slice(sliceNum*n))
+    return result;
+}
+
+solution("abc1Addfggg4556b", 6)
