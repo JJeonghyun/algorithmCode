d2f33ef8f341966fd0fc28c41113176610ae4adb
JJeongHyun <111038274+JJeonghyun@users.noreply.github.com>
2024-07-31 23:38:08 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 62.35 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/README.md
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 대소문자 바꿔서 출력하기 - 181949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181949?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181949) 
 
 ### 성능 요약
 
-메모리: 32.3 MB, 시간: 58.88 ms
+메모리: 32.3 MB, 시간: 62.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 31일 20:23:24
+2024년 07월 31일 23:38:06
 
 ### 문제 설명
 
--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -4,6 +4,10 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+// 왜 형식이 이따구 인지는 모르겠지만
+// 전체 문자열을 반복하면서
+// 대문자로 바꾼거랑 안바꾼거랑 똑같으면 걔는 대문자라는 소리니까 소문자로 바꾸고
+// 아니면 대문자로 바꿈
 function solution(str) {
     let result = '';
     for (let i = 0 ; i < str.length ; i++) {
@@ -19,6 +23,7 @@ rl.on('line', function (line) {
     input.push(line);
 }).on('close', function () {
     const str = input[0];
+    // 이상하게 콘솔찍어야 성공함;;
     console.log(solution(str))
 });
 
