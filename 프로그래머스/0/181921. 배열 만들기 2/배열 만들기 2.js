dd8b909eba48ffd72f70435de36aaf0a7e8d6d2e
JJeongHyun <111038274+JJeonghyun@users.noreply.github.com>
2024-07-31 23:42:14 +0900

    [level 0] Title: 배열 만들기 2, Time: 182.54 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,8 +1,12 @@
 function solution(l, r) {
     const result = []
     for(let i = l ; i <= r ; i++){
+        // 숫자로 뭐 .. 방법없나 하다가 No Time
+        // 숫자를 문자열로 변경하고 하나씩 분리함
         const temp = i.toString().split("")
+        // 분리한 배열이 모두 "5" || "0" 으로 이루어져 있으면 그 숫자를 배열에 넣어버림
         if(temp.every((item)=> item === "5" ||item === "0")) result.push(i)
     }
+    // 결과 배열이 비었으면 [-1] 아니면 예쁘게 잘 넣은 배열 반환
     return result.length ? result : [-1]
 }
