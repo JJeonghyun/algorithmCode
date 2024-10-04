de8ad81fca4be357f45b05c37c827d3c558a6c60
JJeongHyun <111038274+JJeonghyun@users.noreply.github.com>
2024-10-04 19:53:39 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  let tempArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let totalAdd = 0;
+  let answer = 0;
+  tempArr.forEach((item) => {
+    totalAdd += item;
+  });
+  [...numbers].forEach((elem) => {
+    answer += elem;
+  });
+  return totalAdd - answer;
+}
